727fe1dc673869d3874bb96e143def8006e798ff
Eugenemagic <steveji19@gmail.com>
2023-01-31 22:39:22 +0300

    changes

--- sandbox/src/main/java/FirstProgramm.java
+++ sandbox/src/main/java/FirstProgramm.java
@@ -1,6 +1,25 @@
 public class FirstProgramm{
 	public static void main(String[] args){
-		System.out.println("Hello, World");
+		hello("world");
+		hello("Vova");
+		hello("Cap");
+		double len = 5;
+		System.out.println("Площадь квадрата со стороной " + len + " = " + area (len));
+		int a = 4;
+		int b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String somebody){
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double l){
+		return l * l;
+	}
+
+	public static int area (int a, int b){
+		return a + b;
 	}
 }
 
