a57d8724bcd418cd7ff9d98f11f0a0f862bd6a4b
Eugenemagic <steveji19@gmail.com>
2023-01-31 23:05:10 +0300

    class

--- sandbox/src/main/java/FirstProgramm.java
+++ sandbox/src/main/java/FirstProgramm.java
@@ -3,23 +3,18 @@ public class FirstProgramm{
 		hello("world");
 		hello("Vova");
 		hello("Cap");
-		double len = 5;
-		System.out.println("Площадь квадрата со стороной " + len + " = " + area (len));
-		int a = 4;
-		int b = 6;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area(double l){
-		return l * l;
-	}
 
-	public static int area (int a, int b){
-		return a + b;
-	}
+
+
 }
 
