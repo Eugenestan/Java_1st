16c189d23dfed0936508b7c9e470d6154ec577a8
Eugenemagic <steveji19@gmail.com>
2023-02-04 18:49:36 +0300

    driver

---
+++ Webdriver/src/test/java/WebTest.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTest {
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://dtf.ru/");
+    }
+}
--- testaddress/src/test/NewTestCase.java
+++ testaddress/src/test/NewTestCase.java
@@ -1,8 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<TestCaseEntity>
-   <description></description>
-   <name>New Test Case</name>
-   <tag></tag>
-   <comment></comment>
-   <testCaseGuid>d2896a73-01f4-4838-8e51-edf7ef3e780f</testCaseGuid>
-</TestCaseEntity>
+
+public class NewTestCase{
+   public static void main{String[] args}{
+      WebDriver driver = new ChromeDriver();
+      driver.get(www.automationpractice.com/index.php);
+   }
+}
