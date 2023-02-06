7fd3dcf624ecc9a5b0811d8fcc3c0465d10077aa
Eugenemagic <steveji19@gmail.com>
2023-02-06 23:13:12 +0300

    full

--- Webdriver/src/test/java/RecTest.java
+++ Webdriver/src/test/java/RecTest.java
@@ -46,24 +46,41 @@ public class RecTest {
     driver.findElement(By.name("email")).sendKeys("s.evgeniy@topnlab.ru");
     driver.findElement(By.name("password")).sendKeys("54255425");
     driver.findElement(By.xpath("//button[contains(., 'Войти' )]")).click();
-    WebElement dynamicElement = (new WebDriverWait(driver, 100))
+    WebElement waitseller = (new WebDriverWait(driver, 100))
             .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[contains(., 'Объекты')]")));
     driver.findElement(By.xpath("//li[contains(., 'Объекты')]")).click();
-    driver.findElement(By.xpath("//a[contains(., 'Продавцы' )]")).click();
-    WebElement dynamicElement1 = (new WebDriverWait(driver, 100))
+    WebElement waitobject = driver.findElement(By.xpath("//li[contains(., 'Объекты')]"));
+    Actions action = new Actions(driver);
+    action.moveToElement(waitobject).click().perform();
+    driver.findElement(By.xpath("//div[@id='education-header-menu-objects-sale']")).click();
+    WebElement waitcreatebutton = (new WebDriverWait(driver, 100))
             .until(ExpectedConditions.presenceOfElementLocated(By.id("realty-create-button")));
     driver.findElement(By.id("realty-create-button")).click();
-    WebElement dynamicElement2 = (new WebDriverWait(driver, 100))
+    WebElement waitcreatebuttondrop = (new WebDriverWait(driver, 100))
             .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#realty-create-button-dropdown > .item:nth-child(1)")));
     driver.findElement(By.cssSelector("#realty-create-button-dropdown > .item:nth-child(1)")).click();
     driver.findElement(By.cssSelector(".modal__row:nth-child(2) .modal__field")).click();
-    driver.findElement(By.xpath("//input[@formcontrolname='owner_name']")).sendKeys("тест");
+    driver.findElement(By.xpath("//input[@formcontrolname='owner_name']")).sendKeys("проверка1");
     driver.findElement(By.cssSelector("#aboutRealtyPart_Realty_acc .title-wrap")).click();
     driver.findElement(By.cssSelector("#price .modal__field")).click();
     driver.findElement(By.xpath("//input[@formcontrolname='price']")).sendKeys("123");
-    driver.findElement(By.xpath("//div[contains(.,'Служебные поля')]")).click();
-    driver.findElement(By.xpath("//input[@formcontrolname='comment_callcenter']")).sendKeys("12");
-    driver.findElement(By.cssSelector(".mr-0")).click();
-    driver.findElement(By.cssSelector(".modal__btn-block > .\\_red")).click();
+    driver.findElement(By.xpath("//div/div[8][contains(., 'Служебные поля')]")).click();
+    WebElement waitel = driver.findElement(By.xpath("//textarea[@formcontrolname='comment_callcenter']"));
+    Actions action1 = new Actions(driver);
+    action1.moveToElement(waitel).click().perform();
+    driver.findElement(By.xpath("//textarea[@formcontrolname='comment_callcenter']")).sendKeys("12");
+    driver.findElement(By.xpath("//button[contains(., 'Создать')]")).click();
+
+
+    WebElement waitcreatebuttondrop1 = (new WebDriverWait(driver, 5))
+            .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(.,'Сохранить как есть')]")));
+    WebElement waitcreateobject = driver.findElement(By.xpath("//button[contains(.,'Сохранить как есть')]"));
+    Actions action2 = new Actions(driver);
+    action2.moveToElement(waitcreateobject).click().perform();
+    driver.findElement(By.xpath("//button[@class = 're-button _red']"));
+
+
+    WebElement waitcreatebuttondrop2 = (new WebDriverWait(driver, 25))
+            .until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(., 'проверка1')]")));
   }
 }
